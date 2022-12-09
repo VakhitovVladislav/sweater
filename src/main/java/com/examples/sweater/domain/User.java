b04798f3077d53230212254ed072fdf0beb311ce
VakhitovVladislav <67861615+VakhitovVladislav@users.noreply.github.com>
2022-12-09 12:09:48 +0800

    reCaptcha, rest client, rememberMe и сохранение сессий в БД

--- src/main/java/com/examples/sweater/domain/User.java
+++ src/main/java/com/examples/sweater/domain/User.java
@@ -19,18 +19,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
-    }
-
-    @Transient
-    @NotBlank(message =  "Confirm password")
-    private String password2;
     private boolean active;
     @Email(message = "Email is not correct")
     @NotBlank(message = "Email cannot be empty")
--- src/main/resources/templates/parts/login.ftl
+++ src/main/resources/templates/parts/login.ftl
@@ -51,6 +51,14 @@
                 </#if>
         </div>
     </div>
+    <div class="col-sm-6">
+        <div class="g-recaptcha" data-sitekey="6LdmbF8jAAAAAEZ5P7qZfNvDz3m0nO_c1CTzv6dz"></div>
+        <#if captchaError??>
+            <div class="alert alert-danger" role="alert">
+                ${captchaError}
+            </div>
+        </#if>
+    </div>
     </#if>
     <input type="hidden" name="_csrf" value="${_csrf.token}"/>
     <#if !isRegisterForm><a href="/registration">Add new user</a></#if>
